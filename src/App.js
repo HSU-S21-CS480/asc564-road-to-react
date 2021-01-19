deafe78e35559bc89c9c44a4d3d77e601a5045b8
Adam Carter <mail@adam-carter.com>
2021-01-19 14:57:46 -0800

    today's in-class coding activity.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 
+const welcome = {
+  greeting: 'Hey',
+  title: 'React'
+};
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+];
+
+//tutorial from https://github.com/the-road-to-learn-react/the-road-to-react/blob/master/manuscript/react-modern/jsx.md
+const data = ["a", "b", "c", "d"];
+
 function App() {
+  let name = "Adam";
+  let pi = 3.14;
+  pi = "pi";
+
+
+  //never use VAR
+  var some_other_var = "sdfsdf";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        {welcome.greeting} {welcome.title}
+      </h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+      <hr />
+      {list.map((item) => {
+        return (<div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </div>);
+      })}
     </div>
   );
 }
